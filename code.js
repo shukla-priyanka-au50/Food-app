4c4484bc85c61058dba98092c5bbe21c40cdfb53
shukla-priyanka-au50 <94225264+shukla-priyanka-au50@users.noreply.github.com>
2022-12-24 20:08:09 +0530

    Update code.js

--- code.js
+++ code.js
@@ -81,3 +81,59 @@ function highestValuePalindrome(s, n, k) {
     }
     return s.join('');
 }
+
+
+
+
+
+
+
+
+
+
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+
+function largestPrimeFactor(n) {
+  let largestFactor = n;
+  let i = 2;
+  while (i * i <= largestFactor) {
+    while (largestFactor % i === 0) {
+      largestFactor = largestFactor / i;
+    }
+    i++;
+  }
+  return largestFactor;
+}
+
+function main() {
+    var t = parseInt(readLine());
+    for(var a0 = 0; a0 < t; a0++){
+        var n = parseInt(readLine());
+        console.log(largestPrimeFactor(n));
+        
+    }
+
+}
+
